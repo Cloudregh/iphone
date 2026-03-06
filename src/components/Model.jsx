fbcf00e773f33c44c71bb1014681f509fa0074df
Cloudregh <emmacloudre@gmail.com>
2026-03-06 19:29:38 +0000

    model set up and installation of three js

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,32 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap";
 import ModelView from "./ModelView";
+import { useRef, useState } from "react";
+import { yellowImg } from "../utils";
+import * as THREE from 'three';
+
 
 const Model = () => {
+
+    const [size, setSize] = useState('small');
+    const [model, setModel] = useState({
+        title:"iPhone 15 pro in Natural Titanium",
+        color:['#8F8A81', '#FFE7B9', '#6F6C64'],
+        img: yellowImg,
+    })
+
+    // camera control for iphone 15 pro
+    const cameraControlSmall = useRef();
+    const cameraControlLarge = useRef();
+
+    //model
+    const small = useRef(new THREE.Group());
+    const large = useRef(new THREE.Group());
+
+    //rotation
+    const[smallRotation, setSmallRotation] = useState(0);
+    const[largeRotation, setLargeRotation] = useState(0);
+
     useGSAP(() => {
         gsap.to('#heading',{opacity: 1, y: 0, duration: 0.5})
     }, []);
